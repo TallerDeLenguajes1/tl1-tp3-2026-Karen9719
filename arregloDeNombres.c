fb000ef2c34d8ee546f4a2efc9d1f17eb4d86f06
Karen <karenuruea2@gmail.com>
2026-04-27 11:52:15 -0300

    parte 2 punto 2 completado

--- arregloDeNombres.c
+++ arregloDeNombres.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define DIM 15 
 
 void mostrarPersonas(char *nombres[]);
+void buscarNombre(char *nombres[], char palabra[]);
+
 int main(){
     char *nombresDeAlumnos[5]; 
 
@@ -17,6 +20,13 @@ int main(){
 
     }
     mostrarPersonas(nombresDeAlumnos);
+
+    char palabra[DIM];
+
+    printf("Ingrese la palabra a buscar: ");
+    scanf("%s", palabra);
+    buscarNombre(nombresDeAlumnos, palabra);
+
     return 0;
 }
 
@@ -25,4 +35,15 @@ void mostrarPersonas(char *nombres[]){
     for(int i =0; i<5; i++){
         printf("%s, \t ", nombres[i]);
     }
+}
+
+void buscarNombre(char *nombres[], char palabra[]){
+    int i=0;
+    for(int i=0; i<5; i++ ){
+        if(strstr(nombres[i], palabra) != NULL){
+            printf("EL nombre que contiene la palabra ingresada es %s", nombres[i]);
+            return;
+        }
+    }
+    printf("-1"); 
 }
